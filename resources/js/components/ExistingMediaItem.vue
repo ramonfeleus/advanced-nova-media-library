55e772b537071794950bfdc141d83044542e6809
ramonfeleus <73035608+ramonfeleus@users.noreply.github.com>
2022-01-04 14:17:16 +0100

    images gallery fix for non-image

--- resources/js/components/ExistingMediaItem.vue
+++ resources/js/components/ExistingMediaItem.vue
@@ -2,7 +2,7 @@
   <div class="border-40 group px-4 pb-4 mb-4 w-1/6">
     <div class="shadow">
       <div class="overflow-hidden relative w-full" style="padding-top:100%;">
-        <img v-if="'__media_urls__' in item && 'preview' in item.__media_urls__ && item.mime_type.contains('image')" :src="item.__media_urls__.preview" class="absolute block h-full pin-t pin-l w-full" style="object-fit: cover" />
+        <img v-if="'__media_urls__' in item && 'preview' in item.__media_urls__ && item.mimeType().contains('image')" :src="item.__media_urls__.preview" class="absolute block h-full pin-t pin-l w-full" style="object-fit: cover" />
         <button type="button" class="absolute form-file-btn btn btn-default btn-primary pin-t pin-r m-2" @click="$emit('select')">{{__('Select')}}</button>
       </div>
       <div class="p-3 border-l border-r border-b border-40">
